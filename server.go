6b3a8c2d9b1da023e4859e6011659507628f7467
bashery <ahmedalbachiry@gmail.com>
2022-06-26 13:09:03 +0300

    merge functitions

--- server.go
+++ server.go
@@ -26,22 +26,22 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-// deleteBoot update ip addr
+// deleteBoot
 func deleteBoot(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query()
 	serial := url.Get("serial")
 
+	// delete boot from db by serial
 	delete(db, serial)
 
 	fmt.Fprintf(w, "serial : %s\n", serial)
 }
 
-// delete data
+// delete boot from db by serial
 func delete(db *sql.DB, serial string) {
 	stmt, e := db.Prepare("delete from licenses.boots where serial=?")
 	ErrorCheck(e)
 
-	// delete 5th boot
 	_, e = stmt.Exec(serial)
 	ErrorCheck(e)
 }
@@ -52,20 +52,14 @@ func changeIpAddr(w http.ResponseWriter, r *http.Request) {
 	serial := url.Get("serial")
 	ipaddress := url.Get("ip")
 
-	updateIpAddr(db, ipaddress, serial)
-
-	fmt.Fprintf(w, "serial : %s\nipaddress %s", serial, ipaddress)
-}
-
-//updateIpAddr in db
-func updateIpAddr(db *sql.DB, ipaddress, serial string) {
-
 	stmt, e := db.Prepare("update licenses.boots set ipaddress=? where serial=?")
 	ErrorCheck(e)
 
 	// execute
 	_, e = stmt.Exec(ipaddress, serial)
 	ErrorCheck(e)
+
+	fmt.Fprintf(w, "serial : %s\nipaddress %s", serial, ipaddress)
 }
 
 func newBoot(w http.ResponseWriter, r *http.Request) {
@@ -73,13 +67,6 @@ func newBoot(w http.ResponseWriter, r *http.Request) {
 	serial := url.Get("serial")
 	ipaddress := url.Get("ip")
 
-	insert(db, serial, ipaddress)
-
-	fmt.Fprintf(w, "serial : %s\nipaddress %s", serial, ipaddress)
-}
-
-// INSERT INTO DB
-func insert(db *sql.DB, serial, ipaddress string) {
 	// prepare
 	stmt, e := db.Prepare("insert into licenses.boots(serial, ipaddress ) values (?, ?)")
 	ErrorCheck(e)
@@ -87,29 +74,25 @@ func insert(db *sql.DB, serial, ipaddress string) {
 	//execute
 	_, e = stmt.Exec(serial, ipaddress) //,serial())
 	ErrorCheck(e)
+
+	fmt.Fprintf(w, "serial : %s\nipaddress %s", serial, ipaddress)
 }
 
 func expiration(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query()
 	serial := url.Get("serial")
-	time := selectTime(db, serial)
-	fmt.Println("time exp is : ", time)
 
-	fmt.Fprintf(w, "%s", time)
-}
-
-// get time
-func selectTime(db *sql.DB, serial string) string {
 	time := ""
 	row := db.QueryRow("select ts from licenses.boots where serial=?", serial)
 
 	if err := row.Scan(&time); err != nil {
-		fmt.Println("error here", err)
-		return err.Error()
+		ErrorCheck(err)
 	}
-	return time
+
+	fmt.Fprintf(w, "%s", time)
 }
 
+// TODO check this auth func
 // check by serial
 func auth(w http.ResponseWriter, r *http.Request) {
 	// check boot serial if not run on aother ip addres
@@ -120,14 +103,10 @@ func auth(w http.ResponseWriter, r *http.Request) {
 	ip := url.Get("ip")
 	fmt.Println(ip)
 
-	// get remote address
 	addr := r.RemoteAddr
 
 	fmt.Fprintf(w, addr)
 }
-
-//fs := http.FileServer(http.Dir("static/"))
-//http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 // initialaze database
 func initDatabase() *sql.DB {
@@ -153,3 +132,6 @@ func loopping(db *sql.DB) {
 		}
 	}()
 }
+
+//fs := http.FileServer(http.Dir("static/"))
+//http.Handle("/static/", http.StripPrefix("/static/", fs))
